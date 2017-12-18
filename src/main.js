4a276b8d7360bd88bf4b5dc003e1286b15ef4f66
Ramiro Sanchez Balo <ramiro.sanchez@kaleidos.net>
2017-12-18 16:59:54 +0100

    Add new enviorement and main menu

--- src/main.js
+++ src/main.js
@@ -6,3 +6,4 @@ import './custom-reload';
 import { loadScene } from './utils/sceneManipulation';
 
 loadScene('/levels/test.html', '/players/default.html');
+// loadScene('/levels/main-menu.html', '/players/default.html');
