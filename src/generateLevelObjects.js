3ebdea0f8d8554ca77783157628d789d9c4b13c5
Ramiro Sanchez Balo <ramiro.sanchez@kaleidos.net>
2017-12-19 12:56:19 +0100

    Texture test

--- src/generateLevelObjects.js
+++ src/generateLevelObjects.js
@@ -5,30 +5,29 @@ function randomIntFromInterval(min,max) {
 export const generateRandomLevel = () => {
   const entity = document.createElement('a-entity');
 
-  entity.setAttribute('move', 'speed', 4);
+  entity.setAttribute('move', 'speed', 20);
 
-  const objects = randomIntFromInterval(100, 150);
+  const objects = randomIntFromInterval(30, 50);
 
   for (let i = 0; i < objects; i++) {
     const element = document.createElement('a-box');
-
-    element.setAttribute('scale', '0.25 0.25 0.25');
+    
+    const a = randomIntFromInterval(25, 80) / 100;
+    const b = randomIntFromInterval(25, 80) / 100;
+    const c = randomIntFromInterval(100, 200) / 100;
+    const d = a + ' ' + b + ' ' + c;
+    console.log(d);
+    element.setAttribute('scale', d);
+    element.setAttribute('material', 'src: #cubeBlue; repeat: 3 3');
 
     const w = randomIntFromInterval(-100, 100);
     const h = randomIntFromInterval(0, 200);
-    const z = randomIntFromInterval(-10, -100);
+    const z = randomIntFromInterval(-110, -400);
     element.setAttribute('position', `${w / 100} ${h / 100} ${z}`); // w, h, z
-    element.setAttribute('color', 'red');
 
     // random color
     const min = {x: 0, y: 0, z: 0};
     const max = {x: 1, y: 1, z: 1};
-
-    element.setAttribute('material', 'color', '#' + new THREE.Color(
-      Math.random() * max.x + min.x,
-      Math.random() * max.y + min.y,
-      Math.random() * max.z + min.z
-    ).getHexString());
 
     entity.appendChild(element);
   }
