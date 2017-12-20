0708bc2e43a54c170c578bb1f9cd53e6163bbf9b
Juanfran <juanfran.alcantara@kaleidos.net>
2017-12-20 10:36:50 +0100

    init menu levels

--- src/levels/main-menu.js
+++ src/levels/main-menu.js
@@ -9,21 +9,45 @@ AFRAME.registerComponent('main-menu-level', {
             const controller = document.querySelector('.controller-left');
             laser.setAttribute('raycaster', 'objects', '.begin-action');
 
-            const beginBuntton = document.querySelector('.begin-action');
-            const beginBunttonText = beginBuntton.querySelector('a-entity');
+            const beginButton = document.querySelector('.begin-action');
+            const beginButtonText = beginButton.querySelector('a-entity');
 
-            beginBuntton.addEventListener('mouseenter', (e) => {
-                beginBunttonText.setAttribute('material', 'color', 'red');
+            beginButton.addEventListener('mouseenter', (e) => {
+                beginButtonText.setAttribute('material', 'color', 'red');
             });
 
-            beginBuntton.addEventListener('mouseleave', (e) => {
-                beginBunttonText.setAttribute('material', 'color', '#1fd7d5');
+            beginButton.addEventListener('mouseleave', (e) => {
+                beginButtonText.setAttribute('material', 'color', '#1fd7d5');
             });
 
-            beginBuntton.addEventListener('click', (e) => {
+            beginButton.addEventListener('click', (e) => {
               document.querySelector('#menu1').setAttribute('visible', false);
-                // loadScene('/levels/test.html');
+
+              document.querySelector('#menu2').setAttribute('visible', true);
+
+              laser.setAttribute('raycaster', 'objects', '.level-action');
             });
+
+            const levelsButton = document.querySelectorAll('.level-action');
+
+            for (let level of levelsButton) {
+              let button = level.querySelector('a-text');
+
+              button.setAttribute('color', 'red');
+
+              level.addEventListener('mouseenter', (e) => {
+                button.setAttribute('color', 'red');
+              });
+
+              level.addEventListener('mouseleave', (e) => {
+                button.setAttribute('color', 'white');
+              });
+
+              level.addEventListener('click', (e) => {
+                loadScene('/levels/test.html');
+              });
+            }
+
         }, 1000); // TODO WHY?
     }
   });
